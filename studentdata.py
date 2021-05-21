3458eeb6515732d125c0e56cbe931b5653810067
kashish306 <84223438+kashish306@users.noreply.github.com>
2021-05-21 21:38:42 +0530

    By Kashish Rajiv Kumar
    python program which intakes student data

---
+++ studentdata.py
@@ -0,0 +1,34 @@
+import csv
+
+def write_into_csv(info_list):
+    with open('student_info.csv','a',newline='')as csv_file:
+        writer = csv.writer(csv_file)
+
+        if csv_file.tell() == 0:
+            writer.writerow(["Name","Age","Contact Number","Email ID"])
+        writer.writerow(info_list)
+
+if __name__ == '__main__':
+    condition = True
+    student_num = 1
+
+    while(condition):
+        student_info = input("Enter information for student no. #{} in the following format(Name Age Contact_Number Email_ID): ".format(student_num))
+
+        student_info_list = student_info.split(' ')
+
+        print("\nThe entered information is-\nName: {}\nAge: {}\nContact_Number: {}\nEmail_ID: {}".format(student_info_list[0], student_info_list[1], student_info_list[2], student_info_list[3]))
+
+        choice_check = input("Is the entered information correct(yes/no): ")
+
+        if choice_check == "yes":
+            write_into_csv(student_info_list)
+
+            condition_check = input("Enter yes/no if you want to enter infromation for another student: ")
+            if condition_check == "yes":
+                condition = True
+                student_num = student_num + 1
+            elif condition_check == "no":
+                condition = False
+        elif choice_check == "no":
+            print("\n Please re-enter the values")
